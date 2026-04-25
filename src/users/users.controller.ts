0c90a505e3b912a9476d1c3cdc3c65698817ee89
GodsplanElot <elotgodsplan2@gmail.com>
2026-04-25 07:14:28 +0100

    Feat: implement users service with CRUD operations and sample data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,19 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
 
+    constructor(private readonly usersService: UsersService) {}
+
     @Get()// GET /users or /users?role=value
     findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-        return[]
+        return this.usersService.findAll(role);
     }
    
     @Get(':id') //GET /users/:id
     findOne(@Param('id') id: string) {
-        return { id }
+        return this.usersService.findOne(+id);
     }
 
     @Post() // POST /users
@@ -25,7 +28,7 @@ export class UsersController {
 
     @Delete(':id') //DELETE /users/:id
     delete(@Param('id') id: string) {
-        return { id }
+        return this
     }
 
 
