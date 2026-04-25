00f725e853fa2de775b413f55db223455f762462
GodsplanElot <elotgodsplan2@gmail.com>
2026-04-25 14:50:21 +0100

    Feat: add class-transformer and class-validator dependencies, and update CreateUserDto with validation decorators

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from "class-validator";
+
+
 export class CreateUserDto {
-    name: string;
-    email: string;
-    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    
+    @IsString()
+    @IsNotEmpty()
+    name!: string;
+    
+    @IsEmail()
+    email!: string;
+
+    @IsEnum(['INTERN', 'ENGINEER', 'ADMIN'], { message: 'Role must be either INTERN, ENGINEER, or ADMIN' })
+    role!: 'INTERN' | 'ENGINEER' | 'ADMIN';
 }
